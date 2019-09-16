2cbb88f16ef6ef538f7e9178f1f530cd69932d54
Nicolas Fontes <nicolas.rfontes@gmail.com>
2019-09-16 14:52:02 +0100

    Listing 1.3

--- Certification/Certification/Chapter1.cs
+++ Certification/Certification/Chapter1.cs
@@ -5,21 +5,23 @@ namespace Certification
 {
     public class Chapter1
     {
+
+        public static void ThreadMethod(object o)
+        {
+            for (int i = 0; i < (int)o; i++)
+            {
+                Console.WriteLine("ThreadProc:{0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
         public static void Run()
         {
             Console.WriteLine("Chapter 1");
-            Thread t = new Thread(new ThreadStart(ThreadMethod));
-            t.IsBackground = true;
-            t.Start();
-        }
-
-        public static void ThreadMethod()
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine("ThreadProc: {0}", i);
-            Thread.Sleep(1000);
+            Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod));
+            t.Start(5);
+            t.Join();
         }
     }
 }
-}
+
