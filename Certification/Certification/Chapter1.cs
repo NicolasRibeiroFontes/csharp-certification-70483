65d9021954cf058674f55b6b8348a8ca86dfa722
Nicolas Fontes <nicolas.rfontes@gmail.com>
2019-09-16 16:02:33 +0100

    Listing 1.16

--- Certification/Certification/Chapter1.cs
+++ Certification/Certification/Chapter1.cs
@@ -10,20 +10,10 @@ namespace Certification
         public static void Run()
         {
             Console.WriteLine("Chapter 1");
-            Task<int>[] tasks = new Task<int>[3];
-            tasks[0] = Task.Run(() => { Thread.Sleep(2000); return 1; });
-            tasks[1] = Task.Run(() => { Thread.Sleep(1000); return 2; });
-            tasks[2] = Task.Run(() => { Thread.Sleep(3000); return 3; });
+            Parallel.For(0, 10, i => { Thread.Sleep(1000); });
 
-            while (tasks.Length > 0)
-            {
-                int i = Task.WaitAny(tasks);
-                Task<int> completedTask = tasks[i];
-                Console.WriteLine(completedTask.Result);
-                var temp = tasks.ToList();
-                temp.RemoveAt(i);
-                tasks = temp.ToArray();
-            }
+            var numbers = Enumerable.Range(0, 10);
+            Parallel.ForEach(numbers, i => { Thread.Sleep(1000); });
 
         }
     }
