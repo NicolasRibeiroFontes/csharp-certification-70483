7825ad11437026403847dd20c7aafe83d8770ba1
Nicolas Fontes <nicolas.rfontes@gmail.com>
2019-09-16 15:06:15 +0100

    Listing 1.7

--- Certification/Certification/Chapter1.cs
+++ Certification/Certification/Chapter1.cs
@@ -5,27 +5,13 @@ namespace Certification
 {
     public class Chapter1
     {
-        public static ThreadLocal<int> _field = new ThreadLocal<int>(() =>
-        {
-            return Thread.CurrentThread.ManagedThreadId;
-        });
         public static void Run()
         {
             Console.WriteLine("Chapter 1");
-            new Thread(() =>
-            {
-                for (int x = 0; x < _field.Value; x++)
-                {
-                    Console.WriteLine("ThreadA:{0}", x);
-                }
-            }).Start();
-            new Thread(() =>
-            {
-                for (int x = 0; x < _field.Value; x++)
-                {
-                    Console.WriteLine("ThreadB:{0}", x);
-                }
-            }).Start();
+            ThreadPool.QueueUserWorkItem((s) => {
+                Console.WriteLine("Workingona thread from threadpool");
+            });
+
         }
     }
 }
