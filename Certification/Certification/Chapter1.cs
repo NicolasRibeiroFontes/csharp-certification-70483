9ec325681cc743c28990c64f64289598ed86257d
Nicolas Fontes <nicolas.rfontes@gmail.com>
2019-09-16 15:54:05 +0100

    Listing 1.11

--- Certification/Certification/Chapter1.cs
+++ Certification/Certification/Chapter1.cs
@@ -8,13 +8,23 @@ namespace Certification
         public static void Run()
         {
             Console.WriteLine("Chapter 1");
-            Task<int> t = Task.Run(() => {
+            Task<int> t = Task.Run(() =>
+            {
                 return 42;
-            }).ContinueWith((i) => {
-                return i.Result * 2;
             });
-
-            Console.WriteLine(t.Result); // Displays 84
+            t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Canceled");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+            t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Faulted");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+            var completedTask = t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Completed");
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+            completedTask.Wait();
 
 
         }
